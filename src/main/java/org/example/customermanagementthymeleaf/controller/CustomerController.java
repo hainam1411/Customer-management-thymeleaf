10ee7ab8ad3eb7d2a5b9790a38d9c9bf6f243185
hainam1411 <hainam141199@gmail.com>
2024-06-15 22:05:17 +0700

    commit1

--- src/main/java/org/example/customermanagementthymeleaf/controller/CustomerController.java
+++ src/main/java/org/example/customermanagementthymeleaf/controller/CustomerController.java
@@ -6,7 +6,11 @@ import org.example.customermanagementthymeleaf.service.ICustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import java.util.List;
 
@@ -21,4 +25,23 @@ public class CustomerController {
         model.addAttribute("customers", customerList);
         return "/index";
     }
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("customers", new Customer());
+        return "/create";
+    }
+    @PostMapping("/save")
+    public String save(Customer customer, RedirectAttributes redirect ){
+        customer.setId((int) (Math.random()* 10000));
+        customerService.save(customer);
+        redirect.addFlashAttribute("success", " Customer added!");
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String  edit(@PathVariable int id, Model model){
+        model.addAttribute("customer", customerService.findById(id));
+        return "/edit";
+    }
+
 }
